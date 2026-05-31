a9ef819a9417f4fefa589aa354e300e2dbafc89b
mphomosia194 <mphomosia194@gmail.com>
2026-05-31 16:54:38 +0200

    Match expected helper function signature

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Pagination helper function."""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    """Return a tuple containing start and end indexes."""
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of size two containing start and end indexes."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
+
     return (start_index, end_index)
