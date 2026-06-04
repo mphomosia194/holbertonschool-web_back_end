420e05bccb438e19aa8c01bbad1b456321852866
mphomosia194 <mphomosia194@gmail.com>
2026-06-04 10:58:36 +0200

    Comprehension tasks

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module for measuring async comprehension runtime."""
+
+import asyncio
+import time
+
+async_comprehension = __import__(
+    '1-async_comprehension'
+).async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Execute four async comprehensions in parallel and measure runtime."""
+    start_time = time.time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    return time.time() - start_time
